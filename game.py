766911d59769ae141583f6b25d8c1376941f3a8b
tal <tal@tals-MacBook-Air.local>
2022-09-18 13:50:13 +0300

    ball collide paddle - first step finished

--- game.py
+++ game.py
@@ -42,28 +42,40 @@ class Ball():
         self.color    = color 
         self.x_vel = x_vel           
         self.y_vel = y_vel 
+        
+        # for collision with paddle 
+        self.rect = pygame.Rect(self.x, self.y, 2*self.radius, 2*self.radius) 
+        self.rect.center = (self.x, self.y)
+            
+
+    # BUG: not good enough ...  
+    def collide_paddle(self, paddle):
+        if paddle.rect.colliderect(self.rect):
+            ball.y_vel = - ball.y_vel
+        
+
 
     def draw(self): 
-        pygame.draw.circle(WIN, self.color, [self.x,self.y], self.radius)
+        pygame.draw.circle(WIN, self.color, [self.x,self.y], self.radius)   # draw circle (NOT rect) 
+        
         
     def move(self):
         self.x += self.x_vel  
         self.y += self.y_vel 
+        self.rect.x += self.x_vel
+        self.rect.y += self.y_vel
 
 
-    def collision_walls(self): 
+    def collide_walls(self): 
         if ball.y + ball.radius >= WIN_HEIGHT: # button 
             self.y_vel = -self.y_vel 
-            #COLLISION_SOUND.play()
         if self.x + self.radius >= WIN_WIDTH:  # right-wall 
             self.x_vel = -self.x_vel
-            #COLLISION_SOUND.play()
         if self.y - self.radius  <= 0:         # top 
             self.y_vel = -self.y_vel
-            #COLLISION_SOUND.play()
         if self.x - self.radius <= 0 :         # left-wall 
             self.x_vel = -self.x_vel  
-            #COLLISION_SOUND.play()
+            
 
 
 # BALL
@@ -77,9 +89,9 @@ ball = Ball(BALL_POSITION, BALL_RADIUS, BALL_X_VEL, BALL_Y_VEL, BALL_COLOR)
 
 
 
-class Puddle():
+class Paddle():
 
-    def __init__ (self, position, width, height, velocity, color ):
+    def __init__ (self, position, width, height, velocity, color):
 
         self.x = position[0]
         self.y = position[1]
@@ -87,25 +99,32 @@ class Puddle():
         self.width = width 
         self.height = height 
         self.velocity = velocity 
+        
+        # for collision with (the rect of) 'circle' 
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect.topleft = (self.x, self.y)
+
 
     def draw(self):
-        pygame.draw.rect(WIN, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(WIN, self.color, self.rect)
 
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
             self.x += self.velocity
+            self.rect.x += self.velocity
         if keys[pygame.K_LEFT]:
             self.x -= self.velocity
+            self.rect.x -= self.velocity
 
 
 # PADDLE 
-PAD_WIDTH  = 50
-PAD_HEIGHT = 10
-PAD_VEL    = 6
+PAD_WIDTH  = 80
+PAD_HEIGHT = 12
+PAD_VEL    = 7
 PAD_COLOR  = RED 
 PAD_POSITION = (WIN_WIDTH/2 - PAD_WIDTH/2, WIN_HEIGHT - PAD_HEIGHT- 2)
-paddle = Puddle(PAD_POSITION, PAD_WIDTH, PAD_HEIGHT, PAD_VEL, PAD_COLOR)
+paddle = Paddle(PAD_POSITION, PAD_WIDTH, PAD_HEIGHT, PAD_VEL, PAD_COLOR)
 
 
 
@@ -130,10 +149,14 @@ def game():
                 break 
             
             
-        # physics 
-        ball.collision_walls() 
+        # collision 
+        ball.collide_walls()  
+        ball.collide_paddle(paddle)      # improve ...     
+
+        # move 
         ball.move()
         paddle.move()
+        
         
         
         # draw     
